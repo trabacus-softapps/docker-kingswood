2635af88295eb3a9aafe94658fd71bb6c7dc479b
praveen-krishnappa <praveenkumar.krishnappa@softappsit.com>
2017-12-19 11:13:13 +0530

    KA Facilitator Invoice Schedlar only for Deliver Challan

--- additional_addons/kingswood/kw_account_invoice.py
+++ additional_addons/kingswood/kw_account_invoice.py
@@ -2086,11 +2086,16 @@ class account_invoice(osv.osv):
 
             stock_ids_out = stock_obj.search(cr,uid,[('id','in',order_id),('paying_agent_id','not in',dummy_ids),('type','=','out')])
 
-            stock_ids_in = stock_in_obj.search(cr,uid,[('id','in',in_shipment_ids),('partner_id','not in',dummy_ids),('type','=','in')]) 
+            #Commented, Need to revert Back
+            # stock_ids_in = stock_in_obj.search(cr,uid,[('id','in',in_shipment_ids),('partner_id','not in',dummy_ids),('type','=','in')])
+
+            stock_ids_in = []
+            invoice_rate_in = []
 
             invoice_rate_out = stock_obj.get_supplier_rate(cr,uid,stock_ids_out,False,context=context)
-            
-            invoice_rate_in = stock_in_obj.get_supplier_rate(cr,uid,stock_ids_in,False,context=context)
+
+            #Commented, Need to revert Back
+            # invoice_rate_in = stock_in_obj.get_supplier_rate(cr,uid,stock_ids_in,False,context=context)
 
 
             if stock_ids_out:
