b95a30abe322a6eb7b45b5491bba3cb7f23a8f82
praveen-krishnappa <praveenkumar.krishnappa@softappsit.com>
2018-01-29 19:16:04 +0530

    EwayBill

--- additional_addons/kingswood/kw_stock.py
+++ additional_addons/kingswood/kw_stock.py
@@ -1647,13 +1647,13 @@ class stock_picking_out(osv.osv):
                         tax_amount += tx.amount
 
             # For PDF Download Setting options
-            def enable_download_in_headless_chrome(browser, download_dir):
-                #add missing support for chrome "send_command"  to selenium webdriver
-                browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
-
-                params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
-                browser.execute("send_command", params)
-                return browser
+            # def enable_download_in_headless_chrome(browser, download_dir):
+            #     #add missing support for chrome "send_command"  to selenium webdriver
+            #     browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+            #
+            #     params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+            #     browser.execute("send_command", params)
+            #     return browser
 
             chrome_options = Options()
             DOWNLOAD_PATH = '/tmp'
@@ -1690,7 +1690,7 @@ class stock_picking_out(osv.osv):
             # browser = webdriver.Chrome(
             # chrome_options=options)
             # browser.set_window_size(1440, 900)
-            enable_download_in_headless_chrome(browser, DOWNLOAD_PATH)
+            # enable_download_in_headless_chrome(browser, DOWNLOAD_PATH)
 
 
 
