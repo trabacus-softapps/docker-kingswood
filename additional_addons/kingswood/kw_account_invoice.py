51ea7bdacb322819c48971a5d34271744697c1bd
praveen-krishnappa <praveenkumar.krishnappa@softappsit.com>
2018-02-05 12:36:46 +0530

    TN Create Invoice scheduler from 2017-09-01 to 2018-02-02

--- additional_addons/kingswood/kw_account_invoice.py
+++ additional_addons/kingswood/kw_account_invoice.py
@@ -1991,9 +1991,9 @@ class account_invoice(osv.osv):
         # 4. Mohd. Umar
         # Remove these partner
         if fac_state == 'Karnataka':
-            remove_ids = [1,1545,3138,2469]
-        else:
             remove_ids = [0]
+        else:
+            remove_ids = [1572]
         dummy_ids.extend(remove_ids)
 #         print "Date-",shedular_date
         stock_ids_in = []
@@ -2010,7 +2010,7 @@ class account_invoice(osv.osv):
 
         query_out = """select distinct sp.id from stock_picking sp 
                         left outer join res_country_state rs on rs.id = sp.state_id
-                        where sp.date::date >= '2017-07-01' and type = 'out'
+                        where sp.date::date >= '2017-09-01' and type = 'out'
                         and sp.date::date <= '"""+str(shedular_date)+"""'::date and sup_invoice = False
                         and sp.state in ('done','freight_paid') and sp.del_quantity>0
                         and lower(rs.name) ilike '%"""+fac_state+"""%'"""
@@ -2047,7 +2047,7 @@ class account_invoice(osv.osv):
         
         query_in = """select distinct sp.id from stock_picking sp 
                         left outer join res_country_state rs on rs.id = sp.state_id
-                        where sp.date::date >= '2017-07-01' and type = 'in'
+                        where sp.date::date >= '2017-09-01' and type = 'in'
                         and sp.date::date <= '"""+str(shedular_date)+"""'::date and sup_invoice = False                        
                         and sp.state in ('done','freight_paid') and lower(sp.name) ilike '%"""+in_fac_state+"""%'"""
         
@@ -2067,7 +2067,7 @@ class account_invoice(osv.osv):
             #cr.execute("""update stock_picking set user_id = 1 where id in %s""",(tuple(stock_ids_in),))  
         if stock_ids_out or stock_ids_in:
             if stock_ids_out:
-                cr.execute("""select sp.id from stock_picking sp where sp.date::date >= '2017-07-01'::date and sp.id not in (SELECT dr.del_ord_id FROM supp_delivery_invoice_rel dr inner
+                cr.execute("""select sp.id from stock_picking sp where sp.date::date >= '2017-09-01'::date and sp.id not in (SELECT dr.del_ord_id FROM supp_delivery_invoice_rel dr inner
                 join account_invoice ac on ac.id=dr.invoice_id WHERE dr.del_ord_id  IN %s and ac.state <>'cancel') and sp.id in %s""",(tuple(stock_ids_out),tuple(stock_ids_out)))    
 #                 order_id = cr.fetchall()
                 order_id=cr.fetchall()
@@ -2077,7 +2077,7 @@ class account_invoice(osv.osv):
                  
 
             if stock_ids_in:
-                cr.execute("""select sp.id from stock_picking sp where sp.date::date >= '2017-07-01'::date and sp.id not in
+                cr.execute("""select sp.id from stock_picking sp where sp.date::date >= '2017-09-01'::date and sp.id not in
                 (SELECT dr.in_shipment_id FROM incoming_shipment_invoice_rel dr inner
                 join account_invoice ac on ac.id=dr.invoice_id WHERE dr.in_shipment_id  IN  %s and ac.state <>'cancel')and sp.id in %s""",(tuple(stock_ids_in),tuple(stock_ids_in)))    
                 in_shipment_ids = cr.fetchall()
@@ -2351,7 +2351,7 @@ class account_invoice(osv.osv):
         state_id1 = context.get(1,False) 
         state_id2 = context.get(2,False)
         
-        context.update({'shedular_date':'2017-12-20'})
+        context.update({'shedular_date':'2018-02-04'})
 
         _logger.error('Schedular Date.....%s',context)
         res = self.create_facilitator_inv(cr,uid,[uid],context)
