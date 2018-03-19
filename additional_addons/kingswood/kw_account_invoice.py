85959fb49b7d986ebfaf8215f7382c20ec38b424
praveen-krishnappa <praveenkumar.krishnappa@softappsit.com>
2018-03-19 18:55:54 +0530

    KA Scheduler up to 2018-03-18

--- additional_addons/kingswood/kw_account_invoice.py
+++ additional_addons/kingswood/kw_account_invoice.py
@@ -2266,7 +2266,7 @@ class account_invoice(osv.osv):
                                 dc_final_date = dc_final_date[0]
 
                         if dc_final_date =='2018-03-31':
-                            dc_final_date = '2018-03-12'
+                            dc_final_date = '2018-03-18'
                         self.write(cr,uid,[merged_inv],{'date_invoice':dc_final_date,'back_date':True})
                         wf_service.trg_validate(uid, 'account.invoice', merged_inv, 'invoice_open', cr)
                         merged_invoice.append(merged_inv)
@@ -2293,7 +2293,7 @@ class account_invoice(osv.osv):
                                         
                                      
                             billing.update({'st_date':st_date,
-                                            'end_date':'2018-03-12', #today,
+                                            'end_date':'2018-03-18', #today,
                                             'partner_id':partner_id,
                                              
                                             })
@@ -2311,7 +2311,7 @@ class account_invoice(osv.osv):
                         date_start = cr.fetchone()
                         date_start = date_start and date_start[0] or ''                        
                         billing.update({'st_date': date_start,
-                                        'end_date':'2018-03-12', #today,
+                                        'end_date':'2018-03-18', #today,
                                         'partner_id':partner_id,
                                          
                                         })
@@ -2354,7 +2354,7 @@ class account_invoice(osv.osv):
         state_id1 = context.get(1,False) 
         state_id2 = context.get(2,False)
         
-        context.update({'shedular_date':'2018-03-14'})
+        context.update({'shedular_date':'2018-03-20'})
 
         _logger.error('Schedular Date.....%s',context)
         res = self.create_facilitator_inv(cr,uid,[uid],context)
