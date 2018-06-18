39f55ee98043048649e8809e05f74d4386a36146
praveen-krishnappa <praveenkumar.krishnappa@softappsit.com>
2018-06-18 15:32:25 +0530

    KA Facilitator Invoice scheduler for 2018-06-17

--- additional_addons/kingswood/kw_account_invoice.py
+++ additional_addons/kingswood/kw_account_invoice.py
@@ -1991,7 +1991,7 @@ class account_invoice(osv.osv):
         # 4. Mohd. Umar
         # Remove these partner
         if fac_state == 'Karnataka':
-            remove_ids = [1,988,1545]
+            remove_ids = [1,988,1545,2224]
         else:
             remove_ids = [1572,1261,1664]
         dummy_ids.extend(remove_ids)
@@ -2230,7 +2230,7 @@ class account_invoice(osv.osv):
                     except:
                         continue
                     if i_final_date == '2018-06-30':
-                        i_final_date = '2018-06-10'
+                        i_final_date = '2018-06-17'
                     self.write(cr,uid,[invoices_ids[0]],{'date_invoice':i_final_date,'back_date':True})
                     wf_service.trg_validate(uid, 'account.invoice', invoices_ids[0], 'invoice_open', cr) 
                     merged_invoice.append(invoices_ids[0])
@@ -2268,7 +2268,7 @@ class account_invoice(osv.osv):
                                 dc_final_date = dc_final_date[0]
 
                         if dc_final_date =='2018-06-30':
-                            dc_final_date = '2018-06-10'
+                            dc_final_date = '2018-06-17'
                         self.write(cr,uid,[merged_inv],{'date_invoice':dc_final_date,'back_date':True})
                         wf_service.trg_validate(uid, 'account.invoice', merged_inv, 'invoice_open', cr)
                         merged_invoice.append(merged_inv)
@@ -2295,7 +2295,7 @@ class account_invoice(osv.osv):
                                         
                                      
                             billing.update({'st_date':st_date,
-                                            'end_date':'2018-06-10', #today,
+                                            'end_date':'2018-06-17', #today,
                                             'partner_id':partner_id,
                                              
                                             })
@@ -2313,7 +2313,7 @@ class account_invoice(osv.osv):
                         date_start = cr.fetchone()
                         date_start = date_start and date_start[0] or ''                        
                         billing.update({'st_date': date_start,
-                                        'end_date':'2018-06-10', #today,
+                                        'end_date':'2018-06-17', #today,
                                         'partner_id':partner_id,
                                          
                                         })
@@ -2356,7 +2356,7 @@ class account_invoice(osv.osv):
         state_id1 = context.get(1,False) 
         state_id2 = context.get(2,False)
         
-        context.update({'shedular_date':'2018-06-12'})
+        context.update({'shedular_date':'2018-06-19'})
 
         _logger.error('Schedular Date.....%s',context)
         res = self.create_facilitator_inv(cr,uid,[uid],context)
